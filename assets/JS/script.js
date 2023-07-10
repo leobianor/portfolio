1e15e41647f3b6cf0387bb6b60d3aa06f2fcb289
leobianor <89613674+leobianor@users.noreply.github.com>
2023-07-09 21:53:36 -0300

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,66 @@
+const body = document.querySelector("body"),
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector("#check-icon");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode ==="dark"){
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if(getStatus && getStatus ==="close"){
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+        localStorage.setItem("mode", "dark");
+    }else{
+        localStorage.setItem("mode", "light");
+    }
+});
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        localStorage.setItem("status", "close");
+    }else{
+        localStorage.setItem("status", "open");
+    }
+})
+
+// Selecione o switch, o ícone e o elemento de texto
+const switchElement = document.querySelector('.switch');
+const iconElement = document.querySelector('#mode-icon');
+const textElement = document.querySelector('#mode-text');
+
+// Adicione um ouvinte de evento para o switch
+switchElement.addEventListener('click', function() {
+  // Verifique se o ícone atual é "uil-sun"
+  const isSunIcon = iconElement.classList.contains('uil-sun');
+  
+  // Alterne o ícone com base na classe atual
+  if (isSunIcon) {
+    iconElement.classList.remove('uil-sun');
+    iconElement.classList.add('uil-moon');
+  } else {
+    iconElement.classList.remove('uil-moon');
+    iconElement.classList.add('uil-sun');
+  }
+  
+  // Verifique se o texto atual é "Dark Mode"
+  const isDarkMode = textElement.textContent === 'Modo Claro';
+  
+  // Alterne o texto com base no conteúdo atual
+  if (isDarkMode) {
+    textElement.textContent = 'Modo Escuro';
+  } else {
+    textElement.textContent = 'Modo Claro';
+  }
+});
+
+
+
+
